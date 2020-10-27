09d734242dc7dc117d36dd148c08884cf34a9899
Tony Lo <tony.lo.kc@gmail.com>
2020-10-27 00:58:24 -0400

    refactor: adjust style in Content.jsx

--- lib/components/Content.jsx
+++ lib/components/Content.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Content = styled.div`
-  height: calc(${props => props.height} - '56px');
+  height: calc(${props => props.height} - 56px);
   overflow-y: scroll;
   margin-top: 2px;
   padding-top: 6px;
