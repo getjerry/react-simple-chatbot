d6b62820049a0be6e82f55d96f4ad201c35fe506
Tony Lo <tony.lo.kc@gmail.com>
2020-10-27 00:55:32 -0400

    refactor: adjust style in Content.jsx

--- lib/components/Content.jsx
+++ lib/components/Content.jsx
@@ -1,15 +1,10 @@
 import styled from 'styled-components';
 
 const Content = styled.div`
-  height: calc(${props => props.height} - ${props => (props.hideInput ? '56px' : '112px')});
+  height: calc(${props => props.height} - '56px');
   overflow-y: scroll;
   margin-top: 2px;
   padding-top: 6px;
 `;
-
-// Note: removing this from the above styles because we're not using floating and it's affecting our display heights.
-// @media screen and (max-width: 568px) {
-//   height: ${props => (props.floating ? 'calc(100% - 112px)' : '')};
-// }
 
 export default Content;
